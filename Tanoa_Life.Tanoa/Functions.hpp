0db8d537ea3106951a165f75a68bd15481720639
Pyth3rEx <info@pyth3rex.com>
2018-04-26 19:09:59 +0200

    Checkpoint 2

--- Tanoa_Life.Tanoa/Functions.hpp
+++ Tanoa_Life.Tanoa/Functions.hpp
@@ -168,6 +168,7 @@ class Life_Client_Core {
         class unimpound {};
         class vehicleGarage {};
         class wireTransfer {};
+        class insureCar {}
     };
 
     class Functions {
